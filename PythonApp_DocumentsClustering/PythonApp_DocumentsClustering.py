5feef56f31fcbd9af93bddc183c8cae78886adae
grg18120 <up1046903@upnet.gr>
2022-11-24 23:59:20 +0200

    Minor changes

--- PythonApp_DocumentsClustering/PythonApp_DocumentsClustering.py
+++ PythonApp_DocumentsClustering/PythonApp_DocumentsClustering.py
@@ -1,27 +1,106 @@
-# pip install scikit-learn-extra
-# https://scikit-learn-extra.readthedocs.io/en/stable/generated/sklearn_extra.cluster.KMedoids.html
-# https://scikit-learn-extra.readthedocs.io/en/stable/modules/cluster.html#k-medoids
+#Download and import spacy
+#Guide https://stackoverflow.com/questions/36835341/pip-is-not-recognized
+#Error: pip : The term 'pip' is not recognized as the name of a cmdlet, function, script file, or operable program.
+#1)Add pip to Environment Variables 
+#    This PC -> Properties -> Advanced System Settings -> Environment Variables ->
+#    on System variables clik on Path and then Edit -> New ->
+#    Copy/Paste the path where pip exist (C:\Users\George Georgariou\source\repos\PythonApp_DocumentsClustering\PythonApp_DocumentsClustering\VirtualEnv_DocClust\Scripts)
+#2)pip install -U pip setuptools wheel
+#3)pip install -U spacy
 
-from sklearn_extra.cluster import KMedoids
-import numpy as np
-from sklearn.datasets import make_blobs
+#Download trained pipelines (en_core_web_sm, el_core_news_sm)
+#Open terminal here: View -> Terminal
+#python -m spacy download en_core_web_lg
+#python -m spacy download el_core_news_lg
 
-n_samples = 20
-X, labels_true = make_blobs(n_samples=n_samples, random_state=1)
-print(X)
-print(f'Labels Ground Truth = {labels_true}') 
+import spacy
 
-kmedoids = KMedoids(
-    n_clusters = 3, # The number of clusters(medoids) to generate (default= 8) 
-    metric = 'euclidean', # What distance metric to use (default= 'euclidean')
-    method = 'alternate', # Which algorithm to use. 'alternate' is faster while 'pam' is more accurate. (default= 'alternate')
-    init= 'k-medoids++', # medoid initialization method. 'random'(ALTERNATIVE), 'heuristic'(ALTERNATIVE) , 'k-medoids++'(ALTERNATIVE), 'build' (PAM)
-    max_iter = 3, # Maximum number of iterations of algorithm for a single run
-    random_state = 1 # static random medoids (= int number), used to initialise medoids when init='random' 
-)
+#Language object containing all components and data needed to process text.
+nlp = spacy.load('en_core_web_lg')
+nlpGr = spacy.load('el_core_news_lg')
 
-kmedoidsFited= kmedoids.fit(X)
-print(f'Clusters(Medoids) = {kmedoidsFited.cluster_centers_}')
-print(f'Labels prediction = {kmedoidsFited.labels_}') 
-print(f'Sum of distances of samples to their closest cluster center= {kmedoidsFited.inertia_}')
-print(f'Index sto pinaka twn data X ta opoia einai ta medoids pou epilexthikan= {kmedoidsFited.medoid_indices_}')
+#Calling the nlp object on a string of text will return a processed Doc object-container
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion ?. Hellow Worlds")
+
+#This Doc components dont need a trained pipeline to produce below components
+'''
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
+'''
+
+# Keep useful tokens which have Part Of Speech tag POS ['NOUN','PROPN','ADJ']
+'''
+usefullTokensList = [token for token in doc if token.pos_ in ['NOUN','PROPN','ADJ']]
+usefullTokensGenerator = (token for token in doc if token.pos_ in ['NOUN','PROPN','ADJ'])
+usefullTokensSet = {token for token in doc if token.pos_ in ['NOUN','PROPN','ADJ']}
+'''
+
+
+# Doc container-object containes sents attributes (sentences of a document)
+# where each sent in sents has tokens as attributes
+'''
+print(f'Document: {doc}')
+for sent in doc.sents:
+    print(f"Sentensce: {sent}")
+    for token in sent:
+        print(f"        {token}")
+'''
+
+#When we create a Doc object-container, during processing, spaCy first tokenizes the text (token.text)
+#After tokenization, spaCy can parse and tag a given Doc. This is where the trained pipeline and 
+#its statistical models come in, which enable spaCy to make predictions of which tag or label 
+#most likely applies in this context.
+'''
+for token in doc:
+    print(token.text,token.lang_, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+'''
+
+# A named entity is a real world object thats assigned a name for example, a person, acountry
+# a products or a book title. SpaCy can recognize various types of a named entity in a document
+# by asking the model for a prediction. Because models are statical and strongly depend on the
+# examples thew were trained on, this doesnt always work perfectly and might need some tuning
+# later, depending on your case.
+'''
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+spacy.displacy.serve(doc, style="ent") #browser -> http://localhost:5000/
+'''
+
+
+# Token Word vectors - embeddings
+'''
+print("Tokens attributes for vectorizing")
+doc = nlp("Hello")
+for token in doc:
+    print(token.text, token.has_vector, token.vector, token.vector_norm, token.is_oov)
+    print ("--------------Token Vector-------------")
+    print(token.vector)
+'''
+
+# Token Word similarity
+'''
+doc = nlp("Hello Hi")
+word1 = doc[0]
+word2 = doc[1]
+print(f"Words-Tokens similarity: {word1.similarity(word2)}")
+'''
+
+
+# Docoments vectors - embeddings
+'''
+doc = nlp("Hello Hi")
+print("Documents attributes for vectorizing")
+print(doc.has_vector, doc.vector_norm)
+print ("--------------Document Vector-------------")
+print(doc.vector)
+'''
+
+# Documents similarity
+'''
+doc1 = nlp("Hello")
+doc2 = nlp("Hi")
+print(f"Documents similarity: {doc1.similarity(doc2)}")
+'''
+
+
--- PythonApp_DocumentsClustering/external/TFIDF_TfidfVectorizer.py
+++ PythonApp_DocumentsClustering/external/TFIDF_TfidfVectorizer.py
@@ -1,78 +1,106 @@
-from pickletools import float8
-from sklearn.feature_extraction.text import TfidfVectorizer
-import pandas as pd
-import numpy
+#Download and import spacy
+#Guide https://stackoverflow.com/questions/36835341/pip-is-not-recognized
+#Error: pip : The term 'pip' is not recognized as the name of a cmdlet, function, script file, or operable program.
+#1)Add pip to Environment Variables 
+#    This PC -> Properties -> Advanced System Settings -> Environment Variables ->
+#    on System variables clik on Path and then Edit -> New ->
+#    Copy/Paste the path where pip exist (C:\Users\George Georgariou\source\repos\PythonApp_DocumentsClustering\PythonApp_DocumentsClustering\VirtualEnv_DocClust\Scripts)
+#2)pip install -U pip setuptools wheel
+#3)pip install -U spacy
+
+#Download trained pipelines (en_core_web_sm, el_core_news_sm)
+#Open terminal here: View -> Terminal
+#python -m spacy download en_core_web_lg
+#python -m spacy download el_core_news_lg
+
+import spacy
+
+#Language object containing all components and data needed to process text.
+nlp = spacy.load('en_core_web_lg')
+nlpGr = spacy.load('el_core_news_lg')
+
+#Calling the nlp object on a string of text will return a processed Doc object-container
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion ?. Hellow Worlds")
+
+#This Doc components dont need a trained pipeline to produce below components
+'''
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
+'''
+
+# Keep useful tokens which have Part Of Speech tag POS ['NOUN','PROPN','ADJ']
+'''
+usefullTokensList = [token for token in doc if token.pos_ in ['NOUN','PROPN','ADJ']]
+usefullTokensGenerator = (token for token in doc if token.pos_ in ['NOUN','PROPN','ADJ'])
+usefullTokensSet = {token for token in doc if token.pos_ in ['NOUN','PROPN','ADJ']}
+'''
 
 
-corpus = [
-    "The car is driven on the road",
-    "The truck is driven on the highway"
-    ]
+# Doc container-object containes sents attributes (sentences of a document)
+# where each sent in sents has tokens as attributes
+'''
+print(f'Document: {doc}')
+for sent in doc.sents:
+    print(f"Sentensce: {sent}")
+    for token in sent:
+        print(f"        {token}")
+'''
 
-#Initialize Object
-"""
-Pws upologizetai to TFIDF
-tf(t,d) = poses fores emfanizetai to word-t mesa sto document d, 
-        den diairoume me to sunolo twn word tou d
+#When we create a Doc object-container, during processing, spaCy first tokenizes the text (token.text)
+#After tokenization, spaCy can parse and tag a given Doc. This is where the trained pipeline and 
+#its statistical models come in, which enable spaCy to make predictions of which tag or label 
+#most likely applies in this context.
+'''
+for token in doc:
+    print(token.text,token.lang_, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+'''
 
-idf(t,D) = ln ( (1 + D) / (1 + df))
-        D = o arithmos twn documents , count(docouments)
-        df = poses fores emfanizetai to t sto D dhladh
-            An uparxei(den mas noiazei poses fores uparxei) to t sto document[i] tote df++
-
-"""
-vectorizer = TfidfVectorizer(
-    lowercase = True, #all corpus text to lower case
-    
-    #max_df = 1.0, #if a word exist in more than 80% of documents then we remove it
-    
-    #min_df = 1, #if a word exist less than 1 time into corpus, then we remove it
-    
-    use_idf = True, #Use or not idf function 
-    
-    #smooth_idf=True, #True:idf = log(n/(1+df))  False: idf = log(n/df) + 1
-    
-    #sublinear_tf = False, #True: replace tf with 1 + log(tf)
-    
-    #max_features = 10, #how many features it will keep 
-    
-    #stop_words = ['is','the','on'] #Remove string from this list or stop_words = "english" then complete automatically the list with einglish stop words
-    
-    #ngram_range=(1,3) # features na theorountai apo 1 ews 3 words(kathe dunati triada) ['car','car is', 'car is driven', 'driven' , 'driven on', 'driven on the' ....
-    
-    
-    #norm=None 
-    #norm = 'l2' kanoume kanonikopoihsh tou telikou dianusmatos tfidf pou exoume 
-    #   dhladh pernoume to mh kanonikopoihmeno dianusma  kai diairoume kathe timh 
-    #   tou me th posothta Sqrt(Sum(tfidt[i]^2)) = metro tou dianusmatos
-    #   meta tha isxuei Sum(tfidt[i]^2) = 1
-    #   sto sklearn anaferetai oti: The cosine similarity between two vectors is their dot product when l2 norm has been applied.
-    norm=None
-
-    )
+# A named entity is a real world object thats assigned a name for example, a person, acountry
+# a products or a book title. SpaCy can recognize various types of a named entity in a document
+# by asking the model for a prediction. Because models are statical and strongly depend on the
+# examples thew were trained on, this doesnt always work perfectly and might need some tuning
+# later, depending on your case.
+'''
+for ent in doc.ents:
+    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+spacy.displacy.serve(doc, style="ent") #browser -> http://localhost:5000/
+'''
 
 
+# Token Word vectors - embeddings
+'''
+print("Tokens attributes for vectorizing")
+doc = nlp("Hello")
+for token in doc:
+    print(token.text, token.has_vector, token.vector, token.vector_norm, token.is_oov)
+    print ("--------------Token Vector-------------")
+    print(token.vector)
+'''
 
-# Vectors
-docVectors = vectorizer.fit_transform(corpus)
-
-#type:numpy.ndarray | distinct words among documents ['car', 'driven', 'is', ...] 
-featureNames = vectorizer.get_feature_names_out()
-
-#type:tuple | vectrors matrix dimensions ex. (2,8) 2 documents, 8 distict words found in documents 
-vectorMatrixShape = docVectors.shape
-#type: numpy.matrix can convert to list(2D) | document vectors [[0.4, 0.3, 0.0, ...],[0.0, 0.3, 0.4, ...]] 2x8
-dense = docVectors.todense()
-denseList = dense.tolist()
+# Token Word similarity
+'''
+doc = nlp("Hello Hi")
+word1 = doc[0]
+word2 = doc[1]
+print(f"Words-Tokens similarity: {word1.similarity(word2)}")
+'''
 
 
-# Tokenize a specific Document  ['the', 'truck', 'is', 'driven', 'on', 'the', 'highway']
-analyze = vectorizer.build_analyzer()
-tokenizedDocument = analyze("The truck is driven on the highway")
- 
+# Docoments vectors - embeddings
+'''
+doc = nlp("Hello Hi")
+print("Documents attributes for vectorizing")
+print(doc.has_vector, doc.vector_norm)
+print ("--------------Document Vector-------------")
+print(doc.vector)
+'''
 
-tf_idf = pd.DataFrame(docVectors.toarray() ,columns=vectorizer.get_feature_names_out())
-print(tf_idf)
-
+# Documents similarity
+'''
+doc1 = nlp("Hello")
+doc2 = nlp("Hi")
+print(f"Documents similarity: {doc1.similarity(doc2)}")
+'''
 
 
